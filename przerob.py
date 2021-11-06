566a9aebdbf7ecb14b5384f57e5585d999b813f4
Krzysztof Labiak <labiak.krzysztof@gmail.com>
2021-11-06 11:37:02 +0100

    Remove all output print

--- przerob.py
+++ przerob.py
@@ -1,7 +1,4 @@
-#convert txt file in schema 1stline newline empty newline 2ndline newline etc
-#into csv file with 1stline and 2ndline
-#need to convert file to anki
-
+print("--Input file input.txt--")
 f = open("input.txt")
 lines = f.readlines()
 f.close
@@ -11,20 +8,16 @@ out = ""
 i = 0
 
 for line in lines:
-#    print(str(len(line.rstrip())))
     line = line.rstrip()
     linedl = len(line)
     if(linedl > 0 and i == 0):
         temp = line
-        print("temp=" + temp)
         i = 1
     elif(linedl > 0 and i == 1):
         out = out + temp +"; " + line + "\n"
-        print("out=" + out)
         i = 0    
 
-print("----")
+print("--Output file: output.csv--")
 f = open("output.csv","x")
 f.write(out)
 f.close
-#print(out)
