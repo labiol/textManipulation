e5881598f32b59dca2229aff6495dbca8a98b8e5
Krzysztof Labiak <labiak.krzysztof@gmail.com>
2021-11-06 11:55:10 +0100

    rename file, add comment

--- everySecondToCsv.py
+++ everySecondToCsv.py
@@ -1,3 +1,6 @@
+#convert txt file with layout: first_line <newline> empty_line <newline> second_line <newline> third_line etc.
+#into csv file in format: first_line; second_line <newline> third_line; 4th_line <newline> etc.
+
 print("--Input file input.txt--")
 f = open("input.txt")
 lines = f.readlines()
