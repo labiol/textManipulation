2f94d3065f351c25d58981d2cb36e70fad5f2079
Krzysztof Labiak <labiak.krzysztof@gmail.com>
2021-11-06 12:55:16 +0100

    fix comment

--- everySecondToCsv.py
+++ everySecondToCsv.py
@@ -1,4 +1,4 @@
-#convert txt file with layout: first_line <newline> empty_line <newline> second_line <newline> third_line etc.
+#convert txt file with layout: first_line <newline> empty_line <newline> second_line <newline> empty_line etc.
 #into csv file in format: first_line; second_line <newline> third_line; 4th_line <newline> etc.
 
 print("--Input file input.txt--")
